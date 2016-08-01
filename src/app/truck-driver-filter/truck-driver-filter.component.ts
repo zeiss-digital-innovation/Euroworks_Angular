a48d0decce1ed8d152043e3d8b08779bbd21f25a
Stefan Bley <stefan.bley@saxsys.de>
2016-08-01 15:33:39 +0200

    zusammenführen der komponenten

--- src/app/truck-driver-filter/truck-driver-filter.component.ts
+++ src/app/truck-driver-filter/truck-driver-filter.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  selector: 'app-truck-driver-filter',
+  selector: 'ew-truck-driver-filter',
   templateUrl: 'truck-driver-filter.component.html',
   styleUrls: ['truck-driver-filter.component.css']
 })
