10a566bca3258442e6904885dca2616c407a810f
Stefan Bley <stefan.bley@saxsys.de>
2016-08-01 15:23:50 +0200

    ew-header und ew-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import {EwHeaderComponent} from './ew-header/ew-header.component';
+import {EwContentComponent} from './ew-content/ew-content.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [EwHeaderComponent, EwContentComponent],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app works!';
 }
--- src/system-config.ts
+++ src/system-config.ts
@@ -36,6 +36,8 @@ const barrels: string[] = [
   // App specific barrels.
   'app',
   'app/shared',
+  'app/ew-header',
+  'app/ew-content',
   /** @cli-barrel */
 ];
 
