f26637e43d171f4fc053ab1326db3635630df546
Hendrik Lösch <loeschhendrik@googlemail.com>
2016-08-02 14:54:50 +0200

    Gebrochene Spec der EW Combobox gefixt.

--- src/app/ew-combobox/ew-combobox.component.spec.ts
+++ src/app/ew-combobox/ew-combobox.component.spec.ts
@@ -3,11 +3,11 @@
 import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { addProviders, async, inject } from '@angular/core/testing';
-import { EwComboboxComponent } from './ew-combobox.component';
+import { ComboboxComponent } from './ew-combobox.component';
 
 describe('Component: EwCombobox', () => {
   it('should create an instance', () => {
-    let component = new EwComboboxComponent();
+    let component = new ComboboxComponent();
     expect(component).toBeTruthy();
   });
 });
