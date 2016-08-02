f3337f7b5cb494924f8af192b7972890d2542a07
Mauky <manuel.mauky@saxsys.de>
2016-08-02 14:35:08 +0200

    prototyp usage of combobox

--- src/app/truck-driver-filter/truck-driver-filter.component.ts
+++ src/app/truck-driver-filter/truck-driver-filter.component.ts
@@ -1,18 +1,31 @@
 import { Component } from '@angular/core';
 import { LabeledCheckboxComponent } from './labeled-checkbox.component';
+import { ComboboxComponent, ComboboxOption } from '../ew-combobox';
+
 
 @Component({
   moduleId: module.id,
   selector: 'ew-truck-driver-filter',
   templateUrl: 'truck-driver-filter.component.html',
   styleUrls: ['truck-driver-filter.component.css'],
-  directives: [LabeledCheckboxComponent]
+  directives: [LabeledCheckboxComponent, ComboboxComponent]
 })
 export class TruckDriverFilterComponent  {
 
   frachtbrief: boolean = true;
 
- test() {
-   console.log('frachtbrief:' + this.frachtbrief);
- }
+
+	drivingLicenceClasses: Array<ComboboxOption> = [
+		new ComboboxOption("C1"),
+		new ComboboxOption("C"),
+		new ComboboxOption("C1E"),
+		new ComboboxOption("CE")
+	];
+
+	selectedDrivingLicense: string;
+
+
+	test() {
+		console.log('frachtbrief:' + this.frachtbrief);
+	}
 }
